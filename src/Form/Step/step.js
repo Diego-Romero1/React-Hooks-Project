77db7d3cabedbf0f4702a75d298568784eecb46f
Diego Romero <redgo110.krextro@gmail.com>
2023-05-27 22:01:38 -0300

    creating simple hook costume

--- src/Form/Step/step.js
+++ src/Form/Step/step.js
@@ -1,11 +1,15 @@
 import React, { useState, useContext } from "react";
 import { TextField, Button, Box } from "@mui/material";
-import { CounterContext } from "../../Context"
+import { CounterContext } from "../../Context";
+import useAuth from "../../Hooks/useAuth";
 
 const Step = ({ data, step, pasos }) => {
   const { inputs, buttonText, onSubmit } = data;
 
   const counterData = useContext(CounterContext);
+
+  const access = useAuth("counterData.user.jwt");
+  console.log(access);
 
   return (
     <Box
@@ -17,9 +21,9 @@ const Step = ({ data, step, pasos }) => {
         justifyContent: "center",
         flexDirection: "column",
       }}
-      onSubmit={onSubmit}
+      onSubmit={(e) => onSubmit(e, step, pasos)}
     >
-        <strong>Es valor del contador es: {counterData.count}</strong>
+      <strong>El valor del contador es: {counterData.count}</strong>
       {inputs.map((input, i) => {
         const { label, type, value, valid, onChange, helperText, validator } =
           input;
@@ -35,7 +39,7 @@ const Step = ({ data, step, pasos }) => {
             error={valid === false}
             helperText={valid === false && helperText}
             value={value}
-            onChange={(e) => onChange(e,step, i, validator)}
+            onChange={(e) => onChange(e, i, step, validator, pasos)}
           />
         );
       })}
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,60 +1,57 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Typography } from "@mui/material";
-import { LogoSpace, MainSpace, FormSpace, Img } from "./styles";
+import { LogoSpace, FormSpace, Img } from "./styles";
 import DatosUsuario from "./DatosUsuario";
 import DatosPersonales from "./DatosPersonales";
 import DatosEntrega from "./DatosEntrega";
 import Complete from "./Complete";
 import Stepper from "../Stepper";
-import Step from "./Step/step"
+import Step from "./Step/step";
 
-//validations
-import { validarEmail, validarPassword} from "./DatosUsuario/validaciones"
+//Validaciones
+import { validarEmail, validarPassword } from "./DatosUsuario/validaciones";
+import {
+  validarNombre,
+  validarApellidos,
+  validarTelefono,
+} from "./DatosPersonales/validaciones";
+import { validarInput } from "./DatosEntrega/validaciones";
 
 const Form = () => {
-
   const [step, setStep] = useState(0);
-  const [pasos, setPasos] = useState({})
+  const [pasos, setPasos] = useState({});
 
-  const updateStep = (step) => {
-    console.log(step)
-    setStep(step)
-  }
-
-  const onSubmit = (e) => {
-    e.preventDefault()
-    let newStep = step + 1;
-    setStep(newStep);
-    console.log("newStep", newStep);
+  const onSubmit = (e, step, pasos) => {
     console.log(step);
+    e.preventDefault();
+    let newStep = step + 1;
+    console.log(newStep);
+    setStep(newStep);
+    if (newStep === 3) {
+      console.log("Eviar datos al backend", pasos);
+    }
+  };
 
-  }
-
-  const handleChange = (element, currentStep, position, validator) => {
+  const handleChange = (element, position, currentStep, validator, pasos) => {
     const value = element.target.value;
-    const isValid = validator(value);
-    console.log(value);
-    console.log("paso:", currentStep);
-    console.log("posicion:", position);
-    console.log("funcion:", validator);
-    console.log("Es valido?:", isValid);
+    const valid = validator(value);
+    const cp = { ...pasos };
+    cp[currentStep].inputs[position].value = value;
+    cp[currentStep].inputs[position].valid = valid;
 
-    stepsFlow[0].inputs[0].label = "Nombre";
-    stepsFlow[0].inputs[1].label = "no pase perro"
-    console.log(stepsFlow)
-
-  }
+    setPasos(cp);
+  };
 
   const stepsFlow = {
     0: {
       inputs: [
         {
-          label: "Correo Electronico",
+          label: "Correo electrónico",
           type: "email",
           value: "",
           valid: null,
           onChange: handleChange,
-          helpertext: "Ingresar un correo electronico valido.",
+          helperText: "Ingresa un correo electrónico válido.",
           validator: validarEmail,
         },
         {
@@ -63,45 +60,121 @@ const Form = () => {
           value: "",
           valid: null,
           onChange: handleChange,
-          helpertext:  "Ingresa una contraseña válida, Al menos 8 caracteres y máximo 20." ,
-          validator: validarPassword,     
-        }
-      ], 
+          helperText:
+            "Ingresa una contraseña válida, Al menos 8 caracteres y máximo 20.",
+          validator: validarPassword,
+        },
+        {
+          label: "Cuenta de github",
+          type: "text",
+          value: "",
+          valid: null,
+          onChange: handleChange,
+          helperText:
+            "Ingresa una contraseña válida, Al menos 8 caracteres y máximo 20.",
+          validator: validarPassword,
+        },
+      ],
       buttonText: "Siguiente",
-      onSubmit
+      onSubmit,
     },
     1: {
       inputs: [
         {
-          label: "Correo Electronico",
-          type: "email",
+          label: "Nombre",
+          type: "text",
           value: "",
           valid: null,
           onChange: handleChange,
-          helpertext: "Ingresar un correo electronico valido.",
-          validator: validarEmail,
+          helperText: "Ingresa al menos 2 caracteres y máximo 30 caracteres.",
+          validator: validarNombre,
         },
         {
-          label: "Contraseña",
-          type: "password",
+          label: "Apellidos",
+          type: "text",
           value: "",
           valid: null,
           onChange: handleChange,
-          helpertext:  "Ingresa una contraseña válida, Al menos 8 caracteres y máximo 20." ,
-          validator: validarPassword,     
-        }
-      ], 
+          helperText: "Ingresa al menos 2 caracteres y máximo 50 caracteres.",
+          validator: validarApellidos,
+        },
+        {
+          label: "Número telefonico",
+          type: "number",
+          value: "",
+          valid: null,
+          onChange: handleChange,
+          helperText: "Ingresa al menos 8 digitos y máximo 14 digitos.",
+          validator: validarTelefono,
+        },
+      ],
       buttonText: "Siguiente",
-      onSubmit
-    }
-  }
+      onSubmit,
+    },
+    2: {
+      inputs: [
+        {
+          label: "Direccion",
+          type: "text",
+          value: "",
+          valid: null,
+          onChange: handleChange,
+          helperText: "Ingresa al menos 4 caracteres.",
+          validator: validarInput,
+        },
+        {
+          label: "Ciudad",
+          type: "text",
+          value: "",
+          valid: null,
+          onChange: handleChange,
+          helperText: "Ingresa al menos 4 caracteres.",
+          validator: validarInput,
+        },
+        {
+          label: "Estado/Provincia",
+          type: "text",
+          value: "",
+          valid: null,
+          onChange: handleChange,
+          helperText: "Ingresa al menos 4 caracteres.",
+          validator: validarInput,
+        },
+      ],
+      buttonText: "Crear cuenta",
+      onSubmit,
+    },
+  };
+
+  useEffect(() => {
+    setPasos(stepsFlow);
+  }, []);
+
+  // useEffect(async () => {
+  //   try {
+  //     const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+  //     const posts = await data.json();
+  //     console.log(posts);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // });
+
+  //step = 0 --> <DatosUsuario />
+  //step = 1 --> <DatosPersonales />
+  //step = 2 --> <DatosEntrega />
+  //step = 3 --> <Complete />
+
+  const updateStep = (step) => {
+    setStep(step);
+  };
 
   const steps = {
     0: <DatosUsuario updateStep={updateStep} />,
-    1:<DatosPersonales updateStep={updateStep} />,
+    1: <DatosPersonales updateStep={updateStep} />,
     2: <DatosEntrega updateStep={updateStep} />,
-    3: <Complete />
-  }
+    3: <Complete />,
+  };
 
   return (
     <Box
@@ -118,7 +191,10 @@ const Form = () => {
       <FormSpace>
         {step < 3 && <Stepper step={step} />}
         {/* {steps[step]} */}
-        <Step data={stepsFlow[step]}  step={step} />
+        {step < 3 && pasos[step] && (
+          <Step data={pasos[step]} step={step} pasos={pasos} />
+        )}
+        {step === 3 && <Complete />}
       </FormSpace>
     </Box>
   );
